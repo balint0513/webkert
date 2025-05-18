b4afc8d2fd37a9850eedc84e9d53cd5b3f399f54
balint0513 <balint0513@gmail.com>
2025-05-18 20:08:32 +0200

    asd

--- src/app/exams/models/exam.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Exam {
-  id?: string;
-  courseCode: string;
-  courseName: string;
-  examDate: any; // Timestamp vagy Date
-  location: string;
-  enrolledStudents?: string[]; // Neptun kódok listája
-}
